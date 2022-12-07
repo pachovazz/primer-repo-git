94eab5c3e8612174c52dd2879ace92ebd6d8d3d7
Eugenio Zangara <eugeniozangara@gmail.com>
2022-12-06 23:32:27 -0300

    corregido error de repetición de mensajes

--- eugenio_ahorcado.py
+++ eugenio_ahorcado.py
@@ -5,7 +5,7 @@ def inicio():
     animalesDomesticos =("gato", "canario", "perro", "caballo")
     colores= ("azul", "amarillo", "rojo", "negro")
     vidas=7
-    print ("********************************************")
+    print ("**********************************************************")
     print("JUEGO DEL AHORCADO")
     print("tiene 7 vidas, se descontara 1 vida por cada letra incorrecta.")
     print("Puede arriesgar adivinar palabra, en caso incorrecto se le descontaran 3 vidas.") 
@@ -15,9 +15,9 @@ def inicio():
     print("3 Colores")
 
 
-    print ("********************************************")
+    print ("**********************************************************")
     opcion=int(input("ingrese una opción:   "))
-    print ("********************************************")
+    print ("**********************************************************")
     if opcion==1:
         palabra=definirPalabra(animalesSalvajes)
     elif opcion==2:
@@ -51,7 +51,10 @@ def verificarLetra(letra, palabra, respuesta, vidas): #los parámetros son la le
         for indice,letras in enumerate(palabra):
             if letra==letras:
                 respuesta[indice]=letras
-                print("Correcto, la letra ingresada pertenece a la palabra")           
+                coincidencia=True
+                
+        if coincidencia==True:
+            print("Correcto, la letra ingresada pertenece a la palabra")           
         if palabra=="".join(respuesta):
             return (respuesta,True, vidas) 
         else:
@@ -59,22 +62,22 @@ def verificarLetra(letra, palabra, respuesta, vidas): #los parámetros son la le
 
 
 def ingresarLetra():
-    print("*************************************")
+    print ("**********************************************************")
     letra=str(input("Ingrese una letra: ")).upper()
-    print("*************************************")
+    print ("**********************************************************")
     return letra
 
 def adivinarPalabra( intento, palabra, vidas):
        
     if intento==palabra:
-        print(intento,palabra)
+        
         return (palabra, True, vidas)
     else:
         vidas=vidas-3
         print(intento,palabra)
-        print("*************************************")
+        print ("**********************************************************")
         print ("Palabra incorrecta...le restan {} vidas".format(vidas))
-        print("*************************************")
+        print ("**********************************************************")
         return (palabra, False, vidas)
 
 
@@ -93,7 +96,7 @@ def main(palabra):
         respuesta[-1]=palabra[-1]
         print("Palabra: {}".format("".join(respuesta)))
         #print ("Adivinar letra=(1)\nAdivinar Palabra =(2)")
-        print("*************************************")
+        print ("**********************************************************")
         intento=input("Intente adivinar....").upper()
         
         if len(intento)==1:
@@ -103,9 +106,9 @@ def main(palabra):
             respuesta=verificacion[0]
             vidas=verificacion[2]
             if vidas<=0:
-                print("*************************************")
+                print ("**********************************************************")
                 opcion=int(input("Ud. perdió todas las vidas \n Desea jugar de nuevo, ingrese (1)\n Si desea salir, ingrese (2)"))
-                print("*************************************")    
+                print ("**********************************************************")   
                 if opcion==1:
                     inicio()
                 elif opcion==2:
@@ -116,40 +119,24 @@ def main(palabra):
             print (verificacion)
             vidas=verificacion[2]
             if vidas==0:
-                print("*************************************")
+                print ("**********************************************************")
                 opcion=int(input("Ud. perdió todas las vidas \n Desea jugar de nuevo, ingrese (1)\n Si desea salir, ingrese (2)"))
-                print("*************************************")
+                print ("**********************************************************")
                 if opcion==1:
                     inicio()
                 elif opcion==2:
                     break
 
 
-        # opcion=(int(input("Su Opción=??  ")))   
-        # if opcion==1:
-        #     letra=ingresarLetra()
-        #     #print(letra, palabra, respuesta, vidas)
-        #     verificacion=verificarLetra(letra,palabra,respuesta, vidas)           
-        #     respuesta=verificacion[0]
-        #     vidas=verificacion[2]
-        #     if vidas<=0:
-        #         print("*************************************")
-        #         opcion=int(input("Ud. perdió todas las vidas \n Desea jugar de nuevo, ingrese (1)\n Si desea salir, ingrese (2)"))
-        #         print("*************************************")    
-        #         if opcion==1:
-        #             inicio()
-        #         elif opcion==2:
-        #             break
-
                 
         elif opcion==2:
             verificacion=adivinarPalabra(palabra, vidas)
-            print (verificacion)
+            
             vidas=verificacion[2]
             if vidas==0:
-                print("*************************************")
+                print ("**********************************************************")
                 opcion=int(input("Ud. perdió todas las vidas \n Desea jugar de nuevo, ingrese (1)\n Si desea salir, ingrese (2)"))
-                print("*************************************")
+                print ("**********************************************************")
                 if opcion==1:
                     inicio()
                 elif opcion==2:
@@ -157,9 +144,9 @@ def main(palabra):
         else:
             verificacion=(respuesta, False, vidas)
     if verificacion[1] ==True:
-        print("*************************************")
+        print ("**********************************************************")
         print ('Correcto, adívinó la palabra "{}" y finalizó con  {} corazones'.format("".join(palabra), verificacion[2]))  
-        print("*************************************")
+        print ("**********************************************************")
         nuevoJuego=int(input("Desea: \n Jugar de nuevo= (1) \n Salir= (2) \n"))
         if nuevoJuego==1:
             inicio()
